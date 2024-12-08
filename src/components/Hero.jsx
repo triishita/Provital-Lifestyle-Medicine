c89e42c315c262f25ad295c5c83ea112327dff1e
Trishita Yadav <trishitayadav11@gmail.com>
2024-12-09 01:03:58 +0530

    update 4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
-import '../styles/hero.scss';
+import React, { useEffect } from "react";
+import "../styles/hero.scss";
 
-// Import images for the scrolling columns
+// Import images
 import image1 from '../assets/image1.png';
 import image2 from '../assets/image2.png';
 import image3 from '../assets/image3.png';
 import image4 from '../assets/image4.png';
-
+import image5 from '../assets/image5.png';
+import image6 from '../assets/image6.png';
+import image7 from '../assets/image7.png';
+import image8 from '../assets/image8.png';
 
 const Hero = () => {
+  useEffect(() => {
+    // No additional functionality needed; animation handled in CSS.
+  }, []);
+
   return (
     <section className="hero">
-      {/* Static search bar at the top */}
-      <div className="hero-search">
-        <input type="text" placeholder="Condition, procedure, symptoms..." />
+      {/* Search Bar */}
+      <div className="search-bar">
+        <input type="text" placeholder="Condition, procedure, specialty..." />
         <input type="text" placeholder="City, state, or zipcode" />
         <button>Find now</button>
       </div>
 
-      {/* Hero content with images behind it */}
-      <div className="hero-content">
-        <div className="hero-images">
-          <div className="column column-1">
-            <img src={image1} alt="hero1" />
-            <img src={image2} alt="hero2" />
-            <img src={image3} alt="hero3" />
-          </div>
-          <div className="column column-2">
-            <img src={image4} alt="hero4" />
-            <img src={image2} alt="hero5" />
-            <img src={image3} alt="hero6" />
-          </div>
-        </div>
+      {/* Static Text */}
+      <div className="hero-text">
+        <h1>Book an appointment with lifestyle medicine experts</h1>
+        <p>Optimize your lifestyle and reverse chronic diseases.</p>
+      </div>
 
-        <div className="hero-text">
-          <h1>Book an appointment with lifestyle medicine experts</h1>
-          <p>Optimize your lifestyle and reverse chronic diseases.</p>
+      {/* Moving Images */}
+      <div className="image-container">
+        {/* Column 1 */}
+        <div className="image-column column-one">
+          <img src={image1} alt="Image 1" />
+          <img src={image2} alt="Image 2" />
+          <img src={image3} alt="Image 3" />
+          <img src={image4} alt="Image 4" />
+        </div>
+        {/* Column 2 */}
+        <div className="image-column column-two">
+          <img src={image5} alt="Image 5" />
+          <img src={image6} alt="Image 6" />
+          <img src={image7} alt="Image 7" />
+          <img src={image8} alt="Image 8" />
         </div>
       </div>
     </section>
